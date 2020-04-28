0a80eebe6769de2061e98b4f8240e03f1f5f0b15
Yuan QU <quyuan@sibyl-int.com>
2020-04-28 18:28:46 +0800

    try to publish v0.0.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,21 @@
+// export default {
+//     input: 'src/index.js',
+//     output: {
+//       file: 'bundle.js',
+//       format: 'cjs'
+//     }
+//   };
+
+  // rollup.config.js
+const path = require('path');
+const resolve = function (filePath) {
+    return path.join(__dirname, '.', filePath)
+}
 export default {
-    input: 'src/index.js',
-    output: {
-      file: 'bundle.js',
-      format: 'cjs'
+    input: resolve('src/index.js'), // 入口文件
+    output: { // 出口文件
+        file: resolve('dist/bundle.js'),
+        format: 'umd',
+        name:'satellite-geo-calc'
     }
-  };
+};
